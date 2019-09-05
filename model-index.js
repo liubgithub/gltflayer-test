4746c6289b4676d887a483d281de3411e5657f01
liubgithub <471071362@qq.com>
2019-09-05 17:58:42 +0800

    初始化项目

---
+++ model-index.js
@@ -0,0 +1,586 @@
+const gltfModelList = [
+  {
+    "name": "2CylinderEngine",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "2CylinderEngine.gltf",
+      "glTF-Binary": "2CylinderEngine.glb",
+      "glTF-Draco": "2CylinderEngine.gltf",
+      "glTF-Embedded": "2CylinderEngine.gltf",
+      "glTF-pbrSpecularGlossiness": "2CylinderEngine.gltf"
+    }
+  },
+  {
+    "name": "AlphaBlendModeTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "AlphaBlendModeTest.gltf",
+      "glTF-Binary": "AlphaBlendModeTest.glb",
+      "glTF-Embedded": "AlphaBlendModeTest.gltf"
+    }
+  },
+  {
+    "name": "AnimatedCube",
+    "type" : "Further PBR Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "AnimatedCube.gltf"
+    }
+  },
+  {
+    "name": "AnimatedMorphCube",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "AnimatedMorphCube.gltf",
+      "glTF-Binary": "AnimatedMorphCube.glb"
+    }
+  },
+  {
+    "name": "AnimatedMorphSphere",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "AnimatedMorphSphere.gltf",
+      "glTF-Binary": "AnimatedMorphSphere.glb"
+    }
+  },
+  {
+    "name": "AnimatedTriangle",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "AnimatedTriangle.gltf",
+      "glTF-Embedded": "AnimatedTriangle.gltf"
+    }
+  },
+  {
+    "name": "AntiqueCamera",
+    "type" : "Further PBR Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "AntiqueCamera.gltf",
+      "glTF-Binary": "AntiqueCamera.glb"
+    }
+  },
+  {
+    "name": "Avocado",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "Avocado.gltf",
+      "glTF-Binary": "Avocado.glb",
+      "glTF-Draco": "Avocado.gltf",
+      "glTF-pbrSpecularGlossiness": "Avocado.gltf"
+    }
+  },
+  {
+    "name": "BarramundiFish",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "BarramundiFish.gltf",
+      "glTF-Binary": "BarramundiFish.glb",
+      "glTF-Draco": "BarramundiFish.gltf",
+      "glTF-pbrSpecularGlossiness": "BarramundiFish.gltf"
+    }
+  },
+  {
+    "name": "BoomBox",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "BoomBox.gltf",
+      "glTF-Binary": "BoomBox.glb",
+      "glTF-Draco": "BoomBox.gltf",
+      "glTF-pbrSpecularGlossiness": "BoomBox.gltf"
+    }
+  },
+  {
+    "name": "BoomBoxWithAxes",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "BoomBoxWithAxes.gltf"
+    }
+  },
+  {
+    "name": "Box",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "Box.gltf",
+      "glTF-Binary": "Box.glb",
+      "glTF-Draco": "Box.gltf",
+      "glTF-Embedded": "Box.gltf",
+      "glTF-pbrSpecularGlossiness": "Box.gltf"
+    }
+  },
+  {
+    "name": "BoxAnimated",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "BoxAnimated.gltf",
+      "glTF-Binary": "BoxAnimated.glb",
+      "glTF-Embedded": "BoxAnimated.gltf",
+      "glTF-pbrSpecularGlossiness": "BoxAnimated.gltf"
+    }
+  },
+  {
+    "name": "BoxInterleaved",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "BoxInterleaved.gltf",
+      "glTF-Binary": "BoxInterleaved.glb",
+      "glTF-Embedded": "BoxInterleaved.gltf",
+      "glTF-pbrSpecularGlossiness": "BoxInterleaved.gltf"
+    }
+  },
+  {
+    "name": "BoxTextured",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "BoxTextured.gltf",
+      "glTF-Binary": "BoxTextured.glb",
+      "glTF-Embedded": "BoxTextured.gltf",
+      "glTF-pbrSpecularGlossiness": "BoxTextured.gltf"
+    }
+  },
+  {
+    "name": "BoxTexturedNonPowerOfTwo",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "BoxTexturedNonPowerOfTwo.gltf",
+      "glTF-Binary": "BoxTexturedNonPowerOfTwo.glb",
+      "glTF-Embedded": "BoxTexturedNonPowerOfTwo.gltf",
+      "glTF-pbrSpecularGlossiness": "BoxTexturedNonPowerOfTwo.gltf"
+    }
+  },
+  {
+    "name": "BoxVertexColors",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "BoxVertexColors.gltf",
+      "glTF-Binary": "BoxVertexColors.glb",
+      "glTF-Embedded": "BoxVertexColors.gltf"
+    }
+  },
+  {
+    "name": "BrainStem",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "BrainStem.gltf",
+      "glTF-Binary": "BrainStem.glb",
+      "glTF-Draco": "BrainStem.gltf",
+      "glTF-Embedded": "BrainStem.gltf",
+      "glTF-pbrSpecularGlossiness": "BrainStem.gltf"
+    }
+  },
+  {
+    "name": "Buggy",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "Buggy.gltf",
+      "glTF-Binary": "Buggy.glb",
+      "glTF-Draco": "Buggy.gltf",
+      "glTF-Embedded": "Buggy.gltf",
+      "glTF-pbrSpecularGlossiness": "Buggy.gltf"
+    }
+  },
+  {
+    "name": "Cameras",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "Cameras.gltf",
+      "glTF-Embedded": "Cameras.gltf"
+    }
+  },
+  {
+    "name": "CesiumMan",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "CesiumMan.gltf",
+      "glTF-Binary": "CesiumMan.glb",
+      "glTF-Draco": "CesiumMan.gltf",
+      "glTF-Embedded": "CesiumMan.gltf",
+      "glTF-pbrSpecularGlossiness": "CesiumMan.gltf"
+    }
+  },
+  {
+    "name": "CesiumMilkTruck",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "CesiumMilkTruck.gltf",
+      "glTF-Binary": "CesiumMilkTruck.glb",
+      "glTF-Draco": "CesiumMilkTruck.gltf",
+      "glTF-Embedded": "CesiumMilkTruck.gltf",
+      "glTF-pbrSpecularGlossiness": "CesiumMilkTruck.gltf"
+    }
+  },
+  {
+    "name": "Corset",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "Corset.gltf",
+      "glTF-Binary": "Corset.glb",
+      "glTF-Draco": "Corset.gltf",
+      "glTF-pbrSpecularGlossiness": "Corset.gltf"
+    }
+  },
+  {
+    "name": "Cube",
+    "type" : "Further PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "Cube.gltf"
+    }
+  },
+  {
+    "name": "DamagedHelmet",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "DamagedHelmet.gltf",
+      "glTF-Binary": "DamagedHelmet.glb",
+      "glTF-Embedded": "DamagedHelmet.gltf"
+    }
+  },
+  {
+    "name": "Duck",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "Duck.gltf",
+      "glTF-Binary": "Duck.glb",
+      "glTF-Draco": "Duck.gltf",
+      "glTF-Embedded": "Duck.gltf",
+      "glTF-pbrSpecularGlossiness": "Duck.gltf"
+    }
+  },
+  {
+    "name": "EnvironmentTest",
+    "type" : "Extension Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "EnvironmentTest.gltf",
+      "glTF-IBL": "EnvironmentTest.gltf"
+    }
+  },
+  {
+    "name": "FlightHelmet",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "FlightHelmet.gltf"
+    }
+  },
+  {
+    "name": "GearboxAssy",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "GearboxAssy.gltf",
+      "glTF-Binary": "GearboxAssy.glb",
+      "glTF-Draco": "GearboxAssy.gltf",
+      "glTF-Embedded": "GearboxAssy.gltf",
+      "glTF-pbrSpecularGlossiness": "GearboxAssy.gltf"
+    }
+  },
+  {
+    "name": "InterpolationTest",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "InterpolationTest.gltf",
+      "glTF-Binary": "InterpolationTest.glb"
+    }
+  },
+  {
+    "name": "Lantern",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "Lantern.gltf",
+      "glTF-Binary": "Lantern.glb",
+      "glTF-Draco": "Lantern.gltf",
+      "glTF-pbrSpecularGlossiness": "Lantern.gltf"
+    }
+  },
+  {
+    "name": "MetalRoughSpheres",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "MetalRoughSpheres.gltf",
+      "glTF-Binary": "MetalRoughSpheres.glb",
+      "glTF-Embedded": "MetalRoughSpheres.gltf"
+    }
+  },
+  {
+    "name": "Monster",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "Monster.gltf",
+      "glTF-Binary": "Monster.glb",
+      "glTF-Draco": "Monster.gltf",
+      "glTF-Embedded": "Monster.gltf",
+      "glTF-pbrSpecularGlossiness": "Monster.gltf"
+    }
+  },
+  {
+    "name": "MorphPrimitivesTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "MorphPrimitivesTest.gltf",
+      "glTF-Binary": "MorphPrimitivesTest.glb",
+      "glTF-Draco": "MorphPrimitivesTest.glb"
+    }
+  },
+  {
+    "name": "MultiUVTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "MultiUVTest.gltf",
+      "glTF-Binary": "MultiUVTest.glb",
+      "glTF-Embedded": "MultiUVTest.gltf"
+    }
+  },
+  {
+    "name": "NormalTangentMirrorTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "NormalTangentMirrorTest.gltf",
+      "glTF-Binary": "NormalTangentMirrorTest.glb",
+      "glTF-Embedded": "NormalTangentMirrorTest.gltf"
+    }
+  },
+  {
+    "name": "NormalTangentTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "NormalTangentTest.gltf",
+      "glTF-Binary": "NormalTangentTest.glb",
+      "glTF-Embedded": "NormalTangentTest.gltf"
+    }
+  },
+  {
+    "name": "OrientationTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "OrientationTest.gltf",
+      "glTF-Binary": "OrientationTest.glb",
+      "glTF-Embedded": "OrientationTest.gltf"
+    }
+  },
+  {
+    "name": "ReciprocatingSaw",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "ReciprocatingSaw.gltf",
+      "glTF-Binary": "ReciprocatingSaw.glb",
+      "glTF-Draco": "ReciprocatingSaw.gltf",
+      "glTF-Embedded": "ReciprocatingSaw.gltf",
+      "glTF-pbrSpecularGlossiness": "ReciprocatingSaw.gltf"
+    }
+  },
+  {
+    "name": "RiggedFigure",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "RiggedFigure.gltf",
+      "glTF-Binary": "RiggedFigure.glb",
+      "glTF-Draco": "RiggedFigure.gltf",
+      "glTF-Embedded": "RiggedFigure.gltf",
+      "glTF-pbrSpecularGlossiness": "RiggedFigure.gltf"
+    }
+  },
+  {
+    "name": "RiggedSimple",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "RiggedSimple.gltf",
+      "glTF-Binary": "RiggedSimple.glb",
+      "glTF-Draco": "RiggedSimple.gltf",
+      "glTF-Embedded": "RiggedSimple.gltf",
+      "glTF-pbrSpecularGlossiness": "RiggedSimple.gltf"
+    }
+  },
+  {
+    "name": "SciFiHelmet",
+    "type" : "Feature PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "SciFiHelmet.gltf"
+    }
+  },
+  {
+    "name": "SimpleMeshes",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "SimpleMeshes.gltf",
+      "glTF-Embedded": "SimpleMeshes.gltf"
+    }
+  },
+  {
+    "name": "SimpleMorph",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "SimpleMorph.gltf",
+      "glTF-Embedded": "SimpleMorph.gltf"
+    }
+  },
+  {
+    "name": "SimpleSparseAccessor",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "SimpleSparseAccessor.gltf",
+      "glTF-Embedded": "SimpleSparseAccessor.gltf"
+    }
+  },
+  {
+    "name": "SpecGlossVsMetalRough",
+    "type" : "Extension Test Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "SpecGlossVsMetalRough.gltf",
+      "glTF-Binary": "SpecGlossVsMetalRough.glb"
+    }
+  },
+  {
+    "name": "Sponza",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "Sponza.gltf"
+    }
+  },
+  {
+    "name": "Suzanne",
+    "type" : "Further PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "Suzanne.gltf"
+    }
+  },
+  {
+    "name": "TextureCoordinateTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "TextureCoordinateTest.gltf",
+      "glTF-Binary": "TextureCoordinateTest.glb",
+      "glTF-Embedded": "TextureCoordinateTest.gltf"
+    }
+  },
+  {
+    "name": "TextureSettingsTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "TextureSettingsTest.gltf",
+      "glTF-Binary": "TextureSettingsTest.glb",
+      "glTF-Embedded": "TextureSettingsTest.gltf"
+    }
+  },
+  {
+    "name": "TextureTransformTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "TextureTransformTest.gltf"
+    }
+  },
+  {
+    "name": "Triangle",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "Triangle.gltf",
+      "glTF-Embedded": "Triangle.gltf"
+    }
+  },
+  {
+    "name": "TriangleWithoutIndices",
+    "type" : "Simple Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "TriangleWithoutIndices.gltf",
+      "glTF-Embedded": "TriangleWithoutIndices.gltf"
+    }
+  },
+  {
+    "name": "TwoSidedPlane",
+    "type" : "Further PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "TwoSidedPlane.gltf"
+    }
+  },
+  {
+    "name": "UnlitTest",
+    "type" : "Feature Test Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "UnlitTest.gltf",
+      "glTF-Binary": "UnlitTest.glb"
+    }
+  },
+  {
+    "name": "VC",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.gif",
+    "variants": {
+      "glTF": "VC.gltf",
+      "glTF-Binary": "VC.glb",
+      "glTF-Draco": "VC.gltf",
+      "glTF-Embedded": "VC.gltf",
+      "glTF-pbrSpecularGlossiness": "VC.gltf"
+    }
+  },
+  {
+    "name": "VertexColorTest",
+    "type" : "Complex Models",
+    "screenshot": "screenshot/screenshot.png",
+    "variants": {
+      "glTF": "VertexColorTest.gltf",
+      "glTF-Binary": "VertexColorTest.glb",
+      "glTF-Embedded": "VertexColorTest.gltf"
+    }
+  },
+  {
+    "name": "WaterBottle",
+    "type" : "PBR Models",
+    "screenshot": "screenshot/screenshot.jpg",
+    "variants": {
+      "glTF": "WaterBottle.gltf",
+      "glTF-Binary": "WaterBottle.glb",
+      "glTF-Draco": "WaterBottle.gltf",
+      "glTF-pbrSpecularGlossiness": "WaterBottle.gltf"
+    }
+  }
+];
